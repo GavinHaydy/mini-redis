7b70819f6b4961f7d29e59b19b7c64cd7b316288
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-09-03 23:53:53 +0800

    feat: test listten & connect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    println!("Mini Redis listening on 127.0.0.1:6379");
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+        println!("Client connected: {:?}", stream.peer_addr());
+
+        let mut buffer = [0; 1024];
+        let n = stream.read(&mut buffer).unwrap();
+
+        let msg = String::from_utf8_lossy(&buffer[..n]);
+
+        println!("Client received: {}", msg);
+    }
 }
