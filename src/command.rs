faa8c6d6c3d0ada058a48a0354bf3624737411dc
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-09-14 22:48:30 +0800

    feat: add DEL command

--- src/command.rs
+++ src/command.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 pub enum Command {
     Set(String, String),
     Get(String),
+    Del(String),
     Ping,
 }
 
@@ -14,19 +15,14 @@ impl Command {
         }
 
         let name = match &values[0] {
-            RespValue::BulkString(Some(data)) => {
-                String::from_utf8_lossy(data).to_uppercase()
-            }
+            RespValue::BulkString(Some(data)) => String::from_utf8_lossy(data).to_uppercase(),
             _ => return Err("invalid command".to_string()),
         };
 
         match name.as_str() {
             "PING" => {
                 if values.len() != 1 {
-                    return Err(
-                        "ERR wrong number of arguments for 'ping' command"
-                            .to_string(),
-                    );
+                    return Err("ERR wrong number of arguments for 'ping' command".to_string());
                 }
 
                 Ok(Command::Ping)
@@ -34,10 +30,7 @@ impl Command {
 
             "GET" => {
                 if values.len() != 2 {
-                    return Err(
-                        "ERR wrong number of arguments for 'get' command"
-                            .to_string(),
-                    );
+                    return Err("ERR wrong number of arguments for 'get' command".to_string());
                 }
 
                 let key = value_to_string(&values[1])?;
@@ -47,10 +40,7 @@ impl Command {
 
             "SET" => {
                 if values.len() != 3 {
-                    return Err(
-                        "ERR wrong number of arguments for 'set' command"
-                            .to_string(),
-                    );
+                    return Err("ERR wrong number of arguments for 'set' command".to_string());
                 }
 
                 let key = value_to_string(&values[1])?;
@@ -59,60 +49,42 @@ impl Command {
                 Ok(Command::Set(key, value))
             }
 
+            "DEL" => {
+                if values.len() != 2 {
+                    return Err("ERR wrong number of arguments for 'del' command".to_string());
+                }
+
+                let key = value_to_string(&values[1])?;
+
+                Ok(Command::Del(key))
+            }
+
             _ => Err(format!("ERR unknown command '{}'", name)),
         }
     }
-    pub fn execute(
-        self,
-        db: &mut HashMap<String, String>,
-    ) -> RespValue{
+    pub fn execute(self, db: &mut HashMap<String, String>) -> RespValue {
         match self {
             Command::Ping => RespValue::SimpleString("PONG".to_string()),
             Command::Set(key, value) => {
                 db.insert(key, value);
                 RespValue::SimpleString("OK".to_string())
             }
-            Command::Get(key) => {
-                match db.get(&key) {
-                    Some(val) => RespValue::BulkString(Some(val.as_bytes().to_vec())),
-                    None => RespValue::BulkString(None),
-                }
+            Command::Get(key) => match db.get(&key) {
+                Some(val) => RespValue::BulkString(Some(val.as_bytes().to_vec())),
+                None => RespValue::BulkString(None),
+            },
+            Command::Del(key) => {
+                let deleted = db.remove(&key).is_some();
+                RespValue::Integer(if deleted { 1 } else { 0 })
             }
         }
     }
 }
 
-// pub fn execute(
-//     command: Command,
-//     db: &mut HashMap<String, String>,
-// ) -> RespValue {
-//     match command {
-//         Command::Ping => RespValue::SimpleString(String::from("PONG")),
-//         Command::Set { key, value } => {
-//             db.insert(key, value);
-//             RespValue::SimpleString("OK".to_string())
-//         }
-//         Command::Get { key } => {
-//             match db.get(&key) {
-//                 Some(val) => {
-//                     RespValue::BulkString(
-//                         Some(val.as_bytes().to_vec())
-//                     )
-//                 }
-//                 None => {
-//                     RespValue::BulkString(None)
-//                 }
-//             }
-//         }
-//     }
-// }
-
-
 fn value_to_string(value: &RespValue) -> Result<String, String> {
     match value {
         RespValue::BulkString(Some(data)) => {
-            String::from_utf8(data.clone())
-                .map_err(|_| "invalid utf8".to_string())
+            String::from_utf8(data.clone()).map_err(|_| "invalid utf8".to_string())
         }
         RespValue::BulkString(None) => {
             Err("nil bulk string is not valid command argument".to_string())
