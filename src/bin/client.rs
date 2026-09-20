c9ca6160ca615691aa45446a1c8925c8cc8ef371
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2026-09-20 17:21:45 +0800

    feat: add Redis client commands

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,6 +1,7 @@
 use mini_redis::resp;
 use std::io::{Read, Write};
 use std::net::TcpStream;
+use mini_redis::resp::RespValue;
 
 struct Client {
     stream: TcpStream,
@@ -47,16 +48,42 @@ impl Client {
         }
     }
 
-    fn set(&mut self, key: &str, value: &str) -> Result<resp::RespValue, String> {
-        self.send(&["SET", key, value])
+    fn set(&mut self, key: &str, value: &str) -> Result<(), String> {
+        let response = self.send(&["SET", key, value])?;
+
+        match response {
+            resp::RespValue::SimpleString(value) if value == "OK" => {
+                Ok(())
+            }
+            resp::RespValue::Error(err) => {Err(err)}
+            _ => Err("unexpected response ".to_string())
+        }
     }
 
-    fn get(&mut self, key: &str) -> Result<resp::RespValue, String> {
-        self.send(&["GET", key])
+    fn get(&mut self, key: &str) -> Result<Option<String>, String> {
+        let response = self.send(&["GET", key])?;
+
+        match response {
+            resp::RespValue::BulkString(Some(value)) => {
+                let value = String::from_utf8(value)
+                    .map_err(|_| "invalid utf8".to_string())?;
+
+                Ok(Some(value))
+            }
+            resp::RespValue::BulkString(None) => Ok(None),
+            resp::RespValue::Error(e) => Err(e),
+            _ => Err("unexpected response".to_string()),
+        }
     }
 
-    fn del(&mut self, key: &str) -> Result<resp::RespValue, String> {
-        self.send(&["DEL", key])
+    fn del(&mut self, key: &str) -> Result<i64, String> {
+        let response = self.send(&["DEL", key])?;
+
+        match response {
+            resp::RespValue::Integer(value) => Ok(value),
+            resp::RespValue::Error(e) => Err(e),
+            _ => Err("unexpected response".to_string()),
+        }
     }
 
     fn ping(&mut self) -> Result<String, String> {
@@ -96,8 +123,17 @@ fn main() {
     println!("{:?}", client.ping());
 
     println!("{:?}", client.set("name", "Gavin"));
+    match client.set("name", "Gavin") {
+        Ok(()) => println!("set success"),
+        Err(error) => println!("set error: {}", error),
+    }
 
     println!("{:?}", client.get("name"));
+    match client.get("name") {
+        Ok(Some(resp)) => println!("{:?}", resp),
+        Ok(None) => {}
+        Err(e) => println!("{:?}", e),
+    }
 
     println!("{:?}", client.del("name"));
 
